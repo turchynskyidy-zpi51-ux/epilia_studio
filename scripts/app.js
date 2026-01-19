9c4d2ec4c9e1bece7703d2e5c6c2de3001d38f85
croneng <dmytroturchinskiy@gmail.com>
2026-01-19 18:44:10 +0200

    firstForm

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,14 @@
 const contact_us = document.querySelector("#contact");
-const target = document.querySelector("html") 
+const target = document.querySelector("html");
+const background = document.querySelector("#form_background");
+const form = document.querySelector("#contact_form");
 contact_us.addEventListener("click",() => {
-    fetch("./content/forms/contact_us.html")
-    .then(r => r.text())
-    .then(html => {
-      target.insertAdjacentHTML("beforeend", html);
-    })
-    .catch(console.error);
+  form.style.display = "block";
+  background.style.opacity = "0.5";
+  background.style.zIndex = "2";
+})
+background.addEventListener("click", () => {
+  form.style.display = "none";
+  background.style.opacity = "0";
+  background.style.zIndex = "-1";
 })
